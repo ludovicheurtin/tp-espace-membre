d9b1b0774f7c9d14b8712bf55ac132e2409daf11
portable_ludo <rigoludo@gmail.com>
2018-11-23 10:06:39 +0100

    Ajout d'un formulaire mail/pwd/pwdconf non-fonctionnel

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php
+?>
+
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <form action="" method="post">
+        <p><input type="text" name="mail" placeholder="Adresse e-mail"></p>
+        <p><input type="password" name="pwd" placeholder="Mot de passe"></p>
+        <p><input type="password" name="pwdconf" placeholder="Mot de passe identique"></p>
+        <button type="submit">OK</button>
+    </form>
+</body>
+</html>
