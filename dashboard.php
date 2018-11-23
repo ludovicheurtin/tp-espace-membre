af8f20cfb2ed1e7cd457f7875106e9589eb8e059
portable_ludo <rigoludo@gmail.com>
2018-11-23 15:45:00 +0100

    Ajout d'un bouton de deconnexion et d'une page deconnexion.php

--- dashboard.php
+++ dashboard.php
@@ -2,4 +2,19 @@
 session_start();
 var_dump($_SESSION["mail"]);
 var_dump($_SESSION["pwd"]);
-?>
+
+// if(isset($_POST["disconnection"])) {
+//     header("Location: /tp-espace-membre/disconnection.php");
+// }
+?><!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Dashboard</title>
+</head>
+<body>
+<button type="submit" name="disconnection"><a href="disconnection.php">Déconnexion</a></button>
+</body>
+</html>
