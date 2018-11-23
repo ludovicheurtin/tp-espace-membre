8d3088f2c047053dd00366fc60d1e2e320913bec
portable_ludo <rigoludo@gmail.com>
2018-11-23 16:52:36 +0100

    Petites améliorations

--- parametres.php
+++ parametres.php
@@ -1,14 +1,15 @@
-<!DOCTYPE HTML>
-<html>
+<?php
+session_start();
 
+
+?><!DOCTYPE HTML>
+<html>
 <head>
 	<link rel="stylesheet" type="text/css" href="dashboard.css" />
 	<title>Dashboard</title>
 </head>
-
 <h1>Dashboard</h1>
 <p>Bienvenue sur votre espace membre.</p>
-
 <body>
 	<nav>
 		<ul>
@@ -16,13 +17,10 @@
 			<li><a href="parametres.php">Parametres</a></li>
 		</ul>
 	</nav>
-  <p>Changer l'adresse Email</p>
-  <p>Changer le mot de passe</p>
-  <p>Supprimer le compte</p>
-
-  <button type="submit" name="disconnection"><a href="disconnection.php">Déconnexion</a></button>
-     
+  <p><button type="submit" name="change_mail">Changer l'adresse mail</button></p>
+  <p><button type="submit" name="change_pwd">Changer le mot de passe</button></p>
+  <p><button type="submit" name="delete_account">Supprimer le compte</button></p>
+  <button type="submit" name="disconnection"><a href="disconnection.php">Déconnexion</a></button>   
 </body>
-
 </body>
 </html>
